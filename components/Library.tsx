966e046bbb44496cd853d061b6ff47433cbee05c
Aynasiz34 <gomserim@gmail.com>
2023-06-25 01:03:50 +0300

    Upoad modal and functionality implemented

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,9 +2,21 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
   const onClick = () => {
-    //handle upload later
+    if (!user) return authModal.onOpen();
+
+    //TODO check for subscription
+
+    return uploadModal.onOpen();
   };
 
   return (
